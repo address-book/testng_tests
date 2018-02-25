b3318828f6ae90c09500a3cdd6f3050b431ca13d
Titus Fortner <titusfortner@gmail.com>
2018-02-25 13:08:02 -0600

    first attempt at making testng

--- src/test/DriverExamples.java
+++ src/test/DriverExamples.java
@@ -1,7 +1,6 @@
 package test;
 
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -1,9 +1,9 @@
 package test;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SeleniumExamples {
 
