446aee724c292a8ab8ce49f2e4b5ae8cf705d97f
Titus Fortner <titusfortner@gmail.com>
2018-02-25 13:29:59 -0600

    basic selenium script with testng

--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -1,6 +1,7 @@
 package test;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,11 +9,20 @@ import org.testng.annotations.Test;
 public class SeleniumExamples {
 
     @Test
-    public void openBrowser() {
-        System.setProperty("wdm.targetPath", "lib/drivers/auto/");
-        ChromeDriverManager.getInstance().setup();
-
+    public void signIn() {
+        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
+
+        driver.get("http://address-book-example.herokuapp.com");
+        driver.findElement(By.id("sign-in")).click();
+
+        String email = "saucecon@example.com";
+        String password = "password";
+
+        driver.findElement(By.id("session_email")).sendKeys(email);
+        driver.findElement(By.id("session_password")).sendKeys(password);
+        driver.findElement(By.name("commit")).click();
+
         driver.quit();
     }
 
