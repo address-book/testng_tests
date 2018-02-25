290ee82f8f55c343aac0ef96d15eb0b1358cc0bb
Titus Fortner <titusfortner@gmail.com>
2018-02-25 11:38:06 -0800

    implement with Fluent Wait

--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -1,15 +1,23 @@
 package test;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.Test;
+
+import java.util.function.Function;
+
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class SeleniumExamples {
 
     @Test
-    public void signIn() throws InterruptedException {
+    public void signIn() {
         System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
 
@@ -19,9 +27,18 @@ public class SeleniumExamples {
         String email = "saucecon@example.com";
         String password = "password";
 
-        Thread.sleep((2000));
+        Wait fluentWait = new FluentWait(driver)
+                .withTimeout(5, SECONDS)
+                .pollingEvery(500, MICROSECONDS)
+                .ignoring(NoSuchElementException.class);
 
-        driver.findElement(By.id("session_email")).sendKeys(email);
+        WebElement emailElement = (WebElement) fluentWait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver browser) {
+                return browser.findElement(By.id("session_email"));
+            }
+        });
+
+        emailElement.sendKeys(email);
         driver.findElement(By.id("session_password")).sendKeys(password);
         driver.findElement(By.name("commit")).click();
 
