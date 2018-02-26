3e1f9e1139cb2e031e023a57c2abc2b628001380
Titus Fortner <titusfortner@gmail.com>
2018-02-26 14:49:25 -0800

    implement base page

---
+++ src/pages/BasePage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+class BasePage {
+    WebDriver browser;
+
+    void click(By locator) {
+        waitForElement(locator);
+        browser.findElement(locator).click();
+    }
+
+    String getText(By locator) {
+        waitForElement(locator);
+        return browser.findElement(locator).getText();
+    }
+
+    void sendKeys(By locator, String text) {
+        waitForElement(locator);
+        browser.findElement(locator).sendKeys(text);
+    }
+
+    private void waitForElement(By locator) {
+        WebDriverWait wait = new WebDriverWait(browser, 30);
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+}
