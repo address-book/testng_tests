1607395496ef269384f14e751c1ee127a4880332
Titus Fortner <titusfortner@gmail.com>
2018-02-26 11:14:47 -0800

    write the text of the test before implementing

--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -1,32 +1,21 @@
 package test;
 
 import data.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class SeleniumExamples extends BaseTest {
 
     @Test(dataProvider = "validUser", dataProviderClass = UserData.class, retryAnalyzer = test.RetryAnalyzer.class)
     public void signIn(String email, String password) {
-        driver.get("http://address-book-example.herokuapp.com");
-        driver.findElement(By.id("sign-in")).click();
+        HomePage homePage = HomePage.visit(driver);
+        homePage.navigateToSignIn();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.signIn(email, password);
 
-        WebElement emailElement = wait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        By.id("session_email")));
-
-        emailElement.sendKeys(email);
-        driver.findElement(By.id("session_password")).sendKeys(password);
-        driver.findElement(By.name("commit")).click();
-
-        assertEquals("Address Book", driver.getTitle());
+        assertTrue(homePage.isSignedIn());
     }
 
 }
