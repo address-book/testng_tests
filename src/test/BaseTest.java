6cb91ab40a02a2644cc71319d4a22f616a721383
Titus Fortner <titusfortner@gmail.com>
2018-02-25 20:15:25 -0800

    Put configuration data into a yaml

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,17 +11,38 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+
 
 public class BaseTest {
     WebDriver driver;
 
     private String useSauce = System.getenv("USE_SAUCE");
 
+    private static DesiredCapabilities createCapabilities(String value) throws FileNotFoundException, YamlException {
+        YamlReader reader = new YamlReader(new FileReader("src/config/data.yml"));
+        HashMap map = (HashMap) reader.read();
+        HashMap config = (HashMap) map.get(value);
+
+        String browser = config == null ? "chrome" : (String) config.get("browser");
+        String version = config == null ? "63.0" : (String) config.get("version");
+        String platform = config == null ? "macOS 10.12" : (String) config.get("platform");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("platform", platform);
+        capabilities.setCapability("version", version);
+
+        return capabilities;
+    }
+
     @BeforeMethod
-    public void setup(Method method) throws MalformedURLException {
+    public void setup(Method method) throws MalformedURLException, FileNotFoundException, YamlException {
         if (useSauce == null) {
             System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
             driver = new ChromeDriver();
@@ -27,19 +50,10 @@ public class BaseTest {
             String USER = System.getenv("SAUCE_USERNAME");
             String KEY = System.getenv("SAUCE_ACCESS_KEY");
             String URL = "https://" + USER + ":" + KEY + "@ondemand.saucelabs.com/wd/hub";
-            String browserEnv = System.getenv("BROWSER");
-            String platformEnv = System.getenv("PLATFORM");
-            String versionEnv = System.getenv("VERSION");
             String buildEnv = System.getenv("BUILD_TAG");
 
-            String browser = browserEnv == null ? "chrome" : browserEnv;
-            String version = versionEnv == null ? "63.0" : versionEnv;
-            String platform = platformEnv == null ? "macOS 10.12" : platformEnv;
-
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setCapability("browserName", browser);
-            caps.setCapability("platform", platform);
-            caps.setCapability("version", version);
+            String configEnv = System.getenv("PLATFORM_CONFIG");
+            DesiredCapabilities caps = createCapabilities(configEnv);
 
             caps.setCapability("name", method.getName());
             if (buildEnv != null) {
@@ -63,6 +77,5 @@ public class BaseTest {
 
         driver.quit();
     }
+
 }
-
-
