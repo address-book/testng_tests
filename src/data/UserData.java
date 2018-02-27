dcc0cc9136095359f0c34fbad54af21caa6dd8f2
Titus Fortner <titusfortner@gmail.com>
2018-02-27 07:36:06 -0800

    leverage default data to only set what is needed for blank password test

--- src/data/UserData.java
+++ src/data/UserData.java
@@ -5,14 +5,27 @@ import com.github.javafaker.Faker;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class UserData {
         private Faker faker = new Faker();
 
-        public Map<String, String> newUser() {
+    public Map<String, String> newUser() {
             Map<String, String> data = new HashMap<String, String>();
             data.put("emailAddress", faker.internet().emailAddress());
             data.put("password", faker.internet().password());
+            return data;
+        }
+
+        public Map<String, String> newUser(Map<String, String> toMerge) {
+            Map<String, String> data = new HashMap<String, String>();
+            data.put("emailAddress", faker.internet().emailAddress());
+            data.put("password", faker.internet().password());
+
+            Set<String> keys = toMerge.keySet();
+            for (String key : keys) {
+                data.put(key, toMerge.get(key));
+            }
             return data;
         }
 
--- src/test/User.java
+++ src/test/User.java
@@ -5,16 +5,18 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
 
 public class User extends BaseTest {
+    private UserData userData = new UserData();
 
     @Test
     public void signUp() {
         driver.get("http://address-book-example.herokuapp.com/sign_up");
 
-        HashMap newUser = (HashMap) new UserData().newUser();
+        HashMap newUser = (HashMap) userData.newUser();
         String emailAddress = (String) newUser.get("emailAddress");
         String password = (String) newUser.get("password");
 
@@ -23,6 +25,23 @@ public class User extends BaseTest {
         driver.findElement(By.name("commit")).click();
 
         assertEquals("Address Book", driver.getTitle());
+    }
+
+    @Test
+    public void signUpFailBlankPassword() {
+        driver.get("http://address-book-example.herokuapp.com/sign_up");
+
+        Map<String, String> requiredData = new HashMap<String, String>();
+        requiredData.put("password", "");
+        HashMap newUser = (HashMap) userData.newUser(requiredData);
+        String emailAddress = (String) newUser.get("emailAddress");
+        String password = (String) newUser.get("password");
+
+        driver.findElement(By.id("user_email")).sendKeys(emailAddress);
+        driver.findElement(By.id("user_password")).sendKeys(password);
+        driver.findElement(By.name("commit")).click();
+
+        assertEquals("Address Book - Sign Up", driver.getTitle());
     }
 
 }
