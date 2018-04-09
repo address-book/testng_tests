6406f05d587e89364843823ca872bb3c1443ef2a
llaskin <llaskin@gmail.com>
2018-04-09 06:48:03 -0500

    added OpenBrowser2 for slide 30

--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -2,10 +2,21 @@ package test;
 
 import data.UserData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.SignInPage;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class SeleniumExamples extends BaseTest {
@@ -22,7 +33,7 @@ public class SeleniumExamples extends BaseTest {
     }
 
     @Test(dataProvider = "validUser", dataProviderClass = UserData.class, retryAnalyzer = test.RetryAnalyzer.class)
-    public void badSignIn2() throws InterruptedException {
+    public void openBrowser1() throws InterruptedException {
         HomePage homePage = HomePage.visit(driver);
 
         String email = "stpcon@example.com";
@@ -34,5 +45,35 @@ public class SeleniumExamples extends BaseTest {
         driver.findElement(By.id("session_pass")).sendKeys(password);
         driver.findElement(By.name("commit")).click();
 
+    }
+
+    @Test(dataProvider = "validUser", dataProviderClass = UserData.class, retryAnalyzer = test.RetryAnalyzer.class)
+    public void openBrowser2() throws InterruptedException, MalformedURLException {
+        public static final String USERNAME = System.getenv("SAUCE_USERNAME");
+        public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+        public static final String URL = "https://" + USERNAME + ":"
+                + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+        String email = "stpcon@example.com";
+        String password = "example";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "Win 10");
+        caps.setCapability("browserName", "chrome");
+        caps.setCapability("version", "latest");
+
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("http://address-book-example.herokuapp.com");
+
+        driver.findElement(By.id("sign-in")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement emailInput = wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.id("session_email")));
+        emailInput.sendKeys(email);
+        driver.findElement(By.id("session_password")).sendKeys(password);
+        driver.findElement(By.name("commit")).click();
+
+        assertEquals("Address Book - Sign In", driver.getTitle());
     }
 }
